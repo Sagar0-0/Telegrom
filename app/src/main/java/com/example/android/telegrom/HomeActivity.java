fa770ebe6ed8410081ee08de629ec5057ab8f3d5
Sagar <sagar.0dev@gmail.com>
2022-02-21 12:34:04 +0530

    Login Added

--- app/src/main/java/com/example/android/telegrom/HomeActivity.java
+++ app/src/main/java/com/example/android/telegrom/HomeActivity.java
@@ -1,14 +1,82 @@
 package com.example.android.telegrom;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class HomeActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        firebaseAuth= FirebaseAuth.getInstance();
+
+        authStateListener = firebaseAuth -> {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+                // User is signed in
+                Toast.makeText(HomeActivity.this,"Signed In!",Toast.LENGTH_SHORT).show();
+            } else {
+//                onSignedOutCleanup();
+                // User is signed out
+                startSignIn();
+            }
+        };
+
+    }
+
+
+    private void startSignIn(){
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setIsSmartLockEnabled(false)
+                .setAvailableProviders(Arrays.asList(
+                        new AuthUI.IdpConfig.GoogleBuilder().build(),
+                        new AuthUI.IdpConfig.EmailBuilder().build()
+                ))
+                .build();
+
+        signInLauncher.launch(signInIntent);
+    }
+    private ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            result -> {
+                int resultCode=result.getResultCode();
+                if(resultCode==RESULT_OK){
+                    Toast.makeText(HomeActivity.this,"Signed In!",Toast.LENGTH_SHORT).show();
+                }else if(resultCode==RESULT_CANCELED){
+                    Toast.makeText(HomeActivity.this,"Sign in cancelled :(",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            });
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(authStateListener!=null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
--- app/src/main/java/com/example/android/telegrom/SplashScreen.java
+++ app/src/main/java/com/example/android/telegrom/SplashScreen.java
@@ -1,20 +1,28 @@
 package com.example.android.telegrom;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.HandlerThread;
-import android.text.Html;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class SplashScreen extends AppCompatActivity {
 
     ImageView background;
     LottieAnimationView lottieAnimationView;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
